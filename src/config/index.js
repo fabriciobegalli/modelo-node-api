7367dca7237bf068908c100ee94601e1b27f30d1
Fabricio Begalli <fabriciobegalli@gmail.com>
2019-07-14 03:08:29 -0300

    fix(config/index.js): corrigindo estrutura do dotenv.

--- src/config/index.js
+++ src/config/index.js
@@ -3,9 +3,7 @@ module.exports = {
     port: process.env.PORT || 4000,
   },
   db: {
-    connectionString:
-      process.env.MONGODB
-      || 'mongodb+srv://fbegalli:v4d9f@modelo-b7v6b.mongodb.net/test?retryWrites=true&w=majority',
+    connectionString: process.env.MONGO_DB,
   },
   requests: {
     rateLimit: {
